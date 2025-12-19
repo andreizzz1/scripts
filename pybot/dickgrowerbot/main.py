44b008825f219c8b4c6c549b96304aa674c10115
Your Name <you@example.com>
2025-12-19 13:26:45 +0300

    Fix inline message content field

--- pybot/dickgrowerbot/main.py
+++ pybot/dickgrowerbot/main.py
@@ -136,7 +136,9 @@ def _inline_result(
     return InlineQueryResultArticle(
         id=result_id,
         title=title,
-        input_message_content=InputTextMessageContent(text=text, parse_mode=ParseMode.HTML),
+        input_message_content=InputTextMessageContent(
+            message_text=text, parse_mode=ParseMode.HTML
+        ),
         reply_markup=button,
     )
 
